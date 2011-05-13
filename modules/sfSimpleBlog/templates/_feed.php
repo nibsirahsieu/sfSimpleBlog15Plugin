8de88fad66065b4d92aba071ac883d5b42aefac6
nibsirahsieu <nibsirahsieu@gmail.com>
2011-05-14 01:07:01 +0700

    added default format for rss links

--- modules/sfSimpleBlog/templates/_feed.php
+++ modules/sfSimpleBlog/templates/_feed.php
@@ -1,2 +1,2 @@
 <li><?php echo link_to(__('Posts (RSS)'), 'sfSimpleBlog/postsFeed?format=rss') ?></li>
-<li><?php echo link_to(__('Posts (Atom)'), 'sfSimpleBlog/postsFeed') ?></li>
+<li><?php echo link_to(__('Posts (Atom)'), 'sfSimpleBlog/postsFeed?format=atom1') ?></li>
