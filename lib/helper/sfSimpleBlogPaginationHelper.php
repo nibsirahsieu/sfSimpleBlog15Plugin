ae52bccef2c5e3e74c397aff07085953f9b0bc21
nibsirahsieu <nibsirahsieu@gmail.com>
2011-05-17 02:54:08 +0700

    clean up

--- lib/helper/sfSimpleBlogPaginationHelper.php
+++ lib/helper/sfSimpleBlogPaginationHelper.php
@@ -2,8 +2,8 @@
 function page_pagination($pager, $uri, $sf_params, $prev_text = 'Previous', $next_text = 'Next', $sort = 'desc')
 {
   $navigation = '';
- 	if ($pager->haveToPaginate()) {
-  	$prms = array();
+  if ($pager->haveToPaginate()) {
+    $prms = array();
     if (null !== $sf_params && $sf_params instanceof sfParameterHolder) {
       $prms = array_merge($sf_params->getAll(), $prms);
       unset($prms['module'], $prms['action'], $prms['page']);
@@ -12,7 +12,7 @@ function page_pagination($pager, $uri, $sf_params, $prev_text = 'Previous', $nex
 
     $navigation .= '<div id="nav-below" class="navigation">';
     if ($pager->getPage() != 1) {
-    $navigation .= '<div class="nav-next"><a href="'.formatUrlFromParameters($uri.$pager->getPreviousPage(), $prms).'">'.__($next_text).'</a><span class="meta-nav">&rarr;</span></div>';
+      $navigation .= '<div class="nav-next"><a href="'.formatUrlFromParameters($uri.$pager->getPreviousPage(), $prms).'">'.__($next_text).'</a><span class="meta-nav">&rarr;</span></div>';
     }
     if ($pager->getPage() != $pager->getLastPage()) {
       $navigation .= '<div class="nav-previous"><span class="meta-nav">&larr;</span><a href="'.formatUrlFromParameters($uri.$pager->getNextPage(), $prms).'">'.__($prev_text).'</a></div>';
@@ -22,47 +22,12 @@ function page_pagination($pager, $uri, $sf_params, $prev_text = 'Previous', $nex
   return $navigation;
 }
 
-function comment_pagination($pager, $uri, $sf_params, $prev_text = 'Previous', $next_text = 'Next', $sort = 'desc')
-{
-  $navigation = '';
- 	if ($pager->haveToPaginate()) {
-  	$prms = array();
-    if (null !== $sf_params && $sf_params instanceof sfParameterHolder) {
-      $prms = array_merge($sf_params->getAll(), $prms);
-      unset($prms['module'], $prms['action'], $prms['page']);
-    }
-    $uri .= '?comment-page=';
-
-    $navigation .= '<div class="navigation">';
-    if ($sort == 'asc')
-    {
-      if ($pager->getPage() != 1) {
-        $navigation .= '<div class="nav-previous"><span class="meta-nav">&larr;</span><a href="'.formatUrlFromParameters($uri.$pager->getPreviousPage(), $prms).'" class="comment-link">'.__($prev_text).'</a></div>';
-      }
-      if ($pager->getPage() != $pager->getLastPage()) {
-        $navigation .= '<div class="nav-next"><a href="'.formatUrlFromParameters($uri.$pager->getNextPage(), $prms).'" class="comment-link">'.__($next_text).'</a><span class="meta-nav">&rarr;</span></div>';
-      }
-    }
-    else
-    {
-      if ($pager->getPage() != 1) {
-      $navigation .= '<div class="nav-next"><a href="'.formatUrlFromParameters($uri.$pager->getPreviousPage(), $prms).'" class="comment-link">'.__($next_text).'</a><span class="meta-nav">&rarr;</span></div>';
-      }
-      if ($pager->getPage() != $pager->getLastPage()) {
-        $navigation .= '<div class="nav-previous"><span class="meta-nav">&larr;</span><a href="'.formatUrlFromParameters($uri.$pager->getNextPage(), $prms).'" class="comment-link">'.__($prev_text).'</a></div>';
-      }
-    }
-    $navigation .= '</div>';
-  }
-  return $navigation;
-}
-
 function formatUrlFromParameters($uri, $prms)
 {
-	$url = url_for($uri);
-	if (count($prms) > 0)
-	{
-		$url = url_for($url)."?".http_build_query($prms, '', '&');
-	}
-	return $url;
+  $url = url_for($uri);
+  if (count($prms) > 0)
+  {
+    $url = url_for($url)."?".http_build_query($prms, '', '&');
+  }
+  return $url;
 }
