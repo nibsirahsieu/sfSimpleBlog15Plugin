4146c83b2ffabadda4afe0189f16b21fbb425601
nibsirahsieu <nibsirahsieu@gmail.com>
2011-06-01 14:58:24 +0700

    removed _feed.php from sfSimpleBlog

--- modules/sfSimpleBlog/templates/_feed.php
+++
@@ -1,2 +0,0 @@
-<li><?php echo link_to(__('Posts (RSS)'), '@sf_simple_blog_posts_feed?format=rss') ?></li>
-<li><?php echo link_to(__('Posts (Atom)'), '@sf_simple_blog_posts_feed?format=atom1') ?></li>
