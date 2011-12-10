213ac65cf47f35518f336e66f749c06e9f6102c5
nibsirahsieu <nibsirahsieu@gmail.com>
2011-12-10 11:29:35 +0700

    fixed pages navigation

--- modules/sfSimpleBlog/templates/_mainNavigation.php
+++ modules/sfSimpleBlog/templates/_mainNavigation.php
@@ -1,11 +1,16 @@
+<?php $pages = sfSimpleBlogPageQuery::create()->published()->find() ?>
 
-<ul>
-  <li>
-    <a href="<?php echo url_for('@homepage') ?>" title="<?php echo __('Home', '', 'twentyten') ?>"><?php echo __('Home', '', 'twentyten') ?></a>
-  </li>
-  <?php foreach ($pages as $page): ?>
-  <li>
-    <a href="<?php echo url_for('@sf_simple_blog_show_page?page_title='.$page->getStrippedTitle()) ?>" title="<?php echo $page->getName() ?>"><?php echo $page->getName() ?></a>
-  </li>
-  <?php endforeach; ?>
-</ul>
+<div id="globalnav">
+  <div class="menu">
+    <ul>
+      <li>
+        <a href="<?php echo url_for('@homepage') ?>" title="<?php echo __('Home', '', 'twentyten') ?>"><?php echo __('Home', '', 'twentyten') ?></a>
+      </li>
+      <?php foreach ($pages as $page): ?>
+      <li>
+        <a href="<?php echo url_for('@sf_simple_blog_show_page?page_title='.$page->getStrippedTitle()) ?>" title="<?php echo $page->getName() ?>"><?php echo $page->getName() ?></a>
+      </li>
+      <?php endforeach; ?>
+    </ul>
+  </div><!-- #menu -->
+</div><!-- #globalnav -->
