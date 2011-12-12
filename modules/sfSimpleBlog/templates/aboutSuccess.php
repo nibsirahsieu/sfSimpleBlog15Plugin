637ba180959128255480e4cd37baba0ed5662cf9
nibsirahsieu <nibsirahsieu@gmail.com>
2011-12-12 10:16:01 +0700

    removed aboutSuccess.php

--- modules/sfSimpleBlog/templates/aboutSuccess.php
+++
@@ -1,9 +0,0 @@
-<?php $sf_context->getResponse()->setTitle(sfConfig::get('app_sfSimpleBlog_title', 'How is life on earth?').' > '. __('About')) ?>
-
-<div class="post hentry" id="post-<?php echo $about->getId() ?>">
-  <h1 class="entry-title"><?php echo __('About') ?></h1>
-
-  <div class="entry-content">
-    <?php echo $about->getAbout(ESC_RAW) ?>
-  </div>
-</div>
